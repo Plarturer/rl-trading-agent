67d6db328d67f20f06660317387962cb54679b2c
Parker L. Arthur <130910903+Plarturer@users.noreply.github.com>
2026-03-23 10:55:55 +0000

    Initial commit

---
+++ agent.py
@@ -0,0 +1,24 @@
+import numpy as np
+import random
+from collections import deque
+
+class DQNAgent:
+    def __init__(self, state_size, action_size):
+        self.state_size = state_size
+        self.action_size = action_size
+        self.memory = deque(maxlen=2000)
+        self.gamma = 0.95    # discount rate
+        self.epsilon = 1.0  # exploration rate
+        self.epsilon_min = 0.01
+        self.epsilon_decay = 0.995
+        self.learning_rate = 0.001
+
+    def remember(self, state, action, reward, next_state, done):
+        self.memory.append((state, action, reward, next_state, done))
+
+    def act(self, state):
+        if np.random.rand() <= self.epsilon:
+            return random.randrange(self.action_size)
+        return np.argmax(self.model.predict(state)[0])
+
+print("RL Agent initialized for financial market analysis.")
